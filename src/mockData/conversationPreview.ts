689b3fe8ce2c6675601769810c958026771b1d9e
Marcel-Nied <kukielaadam4@gmail.com>
2021-11-24 17:02:18 +0100

    addmockThreeUser

---
+++ src/mockData/conversationPreview.ts
@@ -0,0 +1,20 @@
+import { FaMandalorian } from "react-icons/fa";
+import { ConversationPreview } from "../Interfaces/conversationPreview.interface";
+
+export const MOCKED_MESSAGES: ConversationPreview[] = [
+  {
+    name: "Albert Einstein",
+    text: "Kup ojczulkowi browarka",
+    avatarUrl: "Albert_Einstein_Head.jpg",
+  },
+  {
+    name: "Maria Curie-Składowska",
+    text: "Ani się waż ojcu kupować alkohol",
+    avatarUrl: "Maria.jpg",
+  },
+  {
+    name: "Adam Małysz",
+    text: "ale zadyma w domu",
+    avatarUrl: "adam-malysz.jpg",
+  },
+];
