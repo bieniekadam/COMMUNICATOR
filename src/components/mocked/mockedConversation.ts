4843d8565e9d71d4d49e4ad4403cf4c683c13892
Adam Bieniek <adambieniek@MacBook-Air-Adam.local>
2021-11-29 13:22:04 +0100

    scroll feature added in conversation window

---
+++ src/components/mocked/mockedConversation.ts
@@ -0,0 +1,184 @@
+import { Message } from "../conversation/conversation";
+
+export const messages: Message[] = [
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+  {
+    author: 12,
+    text: "aspdfoij",
+  },
+  {
+    author: 13,
+    text: "qwerrzrzrzrrzrzr",
+  },
+  {
+    author: 12,
+    text: "zxcv",
+  },
+];
