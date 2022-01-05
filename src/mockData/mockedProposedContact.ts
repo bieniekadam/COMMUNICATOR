a32782a8e1a0b6d38a00599bcf29003a4b9a3135
Marcel-Nied <kukielaadam4@gmail.com>
2022-01-05 12:29:08 +0100

    Searching contact in Popup

---
+++ src/mockData/mockedProposedContact.ts
@@ -0,0 +1,51 @@
+import { ConversationPreviewData } from "../Interfaces/conversationPreviewProps.interface";
+
+export const PROPOSED_CONTACTS: ConversationPreviewData[] = [
+  {
+    name: "Stachu Jones",
+    text: "Wzbudzuliście moją litość",
+    avatarUrl: "Stachu Jones.jpg",
+    conversationId: 4,
+  },
+
+  {
+    name: "Jurek Owsiak",
+    text: "Nic nie wiem o żadnych zbrodniach",
+    avatarUrl: "Jurek Owsiak.jpg",
+    conversationId: 5,
+  },
+
+  {
+    name: "Piotr Łuszcz",
+    text: "jestem bogiem i ty też jesteś bogiem",
+    avatarUrl: "Piotr Łuszcz.jpg",
+    conversationId: 6,
+  },
+
+  {
+    name: "Zbnigniew Stonoga",
+    text: "Wynik kampani KW Stonogi to jest jakaś porażka",
+    avatarUrl: "Zbigniew Stonoga.jpg",
+    conversationId: 7,
+  },
+
+  {
+    name: "Jarosław Kaczyński",
+    text: "Nikt nie powie nam, że białe jest białe a czarne jest czarne",
+    avatarUrl: "Jarosław Kaczyński.jpeg",
+    conversationId: 8,
+  },
+
+  {
+    name: "Mateusz Hądzlik",
+    text: "Bóg",
+    avatarUrl: "Mateusz Hądzlik.jpg",
+    conversationId: 9,
+  },
+  {
+    avatarUrl: "sc.png",
+    name: "Karol Wojtyła",
+    text: "Nie lękaj się przejśc przez bramę, która jest Chrystus",
+    conversationId: 10,
+  },
+];
